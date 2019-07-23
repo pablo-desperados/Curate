daf6c999936d75794fb16204a1fe7feef64171cd
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-07-23 08:31:28 -0400

    Fixed tests

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,24 +6,25 @@ feature 'user signs in', %Q{
   So that I can regain access to my account
 } do
   scenario 'specify valid credentials' do
-    user = FactoryBot.create(:user)
-
+    user = User.create(
+      first_name: "John",
+      last_name: "Smith",
+      email: "jsmith@email.com",
+      password: "password")
     visit new_user_session_path
 
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
     click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully')
-    expect(page).to have_content('Sign Out')
+    expect(page).to have_content('SIGN OUT')
+    expect(page).to have_content('Create a story')
   end
 
   scenario 'specify invalid credentials' do
     visit new_user_session_path
 
     click_button 'Log in'
-    expect(page).to have_content('Invalid Email or password')
     expect(page).to_not have_content('Sign Out')
   end
 end
