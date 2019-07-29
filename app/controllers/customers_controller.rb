e1f8c78204c893590f2a7d0c4b99b6532fb10281
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-07-29 14:33:00 -0400

    -Added customer model validations -Fixed css problems with customer index page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,14 +1,28 @@
 class CustomersController < ApplicationController
   before_action :authenticate_user
-  def index
+
+  def create
+    customer = Customer.new(customer_params)
+    binding.pry
+    if customer.save
+      binding.pry
+    end
   end
 
+  def new
+    @customer = Customer.new
+  end
 
   def authenticate_user
     if !user_signed_in?
       flash[:notice]="You don't have access to this page. Please sign in."
       redirect_to new_user_registration_path
     end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :lifecycle_status, :phone_number, :title, :company_name, :location, :profile_picture)
   end
 
 end
