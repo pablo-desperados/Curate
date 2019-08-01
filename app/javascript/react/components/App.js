05218b0e220b903adab6364f139a4781be43e278
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-08-01 14:30:27 -0400

    -Added Swtich to react routers

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import CustomersContainer from '../containers/CustomersContainer'
+import CustomerShowTileContainer from '../containers/CustomerShowTileContainer'
 
 const App = (props) => {
   return (
     <BrowserRouter>
+        <Switch>
         <Route exact path="/users/:id/customers" component={CustomersContainer} />
+        <Route exact path="/users/:id/customers/:id" component={CustomerShowTileContainer} />
+        </Switch>
     </BrowserRouter>
   )
 }
---
+++ app/javascript/react/containers/CustomerShowTileContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class CustomerShowTileContainer extends React.component{
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+
+    }
+
+  render(){
+    return(
+      <div>
+        <h1>test</h1>
+      </div>
+    )
+
+  }
+
+}
+
+export default CustomersShowTileContainer
