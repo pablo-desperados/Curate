e8ae0dc66493b34ebd26f7b372e739f6df7b2287
pablo mujica <pablomujica@pablos-MacBook-Pro-2.local>
2019-08-14 14:28:29 -0400

    Reestrcutured the hubspot redirect URI

--- app/controllers/hubspot_controller.rb
+++ app/controllers/hubspot_controller.rb
@@ -6,14 +6,14 @@ require 'json'
     @access_code = get_access(params["code"])
     @access_information = get_info(@access_code)
     create_customers(@access_information)
-    redirect_to  user_customers_path(current_user.id)
+    redirect_to  user_customers_path(current_user)
   end
 
 
   private
   def get_access(token_code)
 
-    @response = HTTParty.post("https://api.hubapi.com/oauth/v1/token?grant_type=authorization_code&client_id=#{ENV["HUBSPOT"]}&client_secret=#{ENV["SECRET_KEY"]}&redirect_uri=https://curate-io.herokuapp.com/oauth-callback/&scope=contacts%20oauth&code=#{token_code}")
+    @response = HTTParty.post("https://api.hubapi.com/oauth/v1/token?grant_type=authorization_code&client_id=#{ENV["HUBSPOT"]}&client_secret=#{ENV["SECRET_KEY"]}&redirect_uri=https://curate-io.herokuapp.com/oauth-callback&code=#{token_code}")
 
   end
 
